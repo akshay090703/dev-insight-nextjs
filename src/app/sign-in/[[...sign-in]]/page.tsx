a838440a2973a2379e8146f515eff694c8917a42
akshay090703 <3551akshay@gmail.com>
2024-12-25 00:53:32 +0530

    clerk authentication and shadcn setup

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,25 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+    return (
+        <section className="bg-white">
+            <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+                <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+                    <img
+                        alt=""
+                        src="https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        className="absolute inset-0 h-full w-full object-cover opacity-80"
+                    />
+                </section>
+
+                <main
+                    className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+                >
+                    <div className="max-w-xl lg:max-w-3xl">
+                        <SignIn />
+                    </div>
+                </main>
+            </div>
+        </section>
+    )
+}
