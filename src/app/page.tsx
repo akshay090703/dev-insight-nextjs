13f32a20810441abbf91e7ae93cfe1a5a6e0328d
akshay090703 <3551akshay@gmail.com>
2025-01-03 22:56:14 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button";
-
+import { redirect } from 'next/navigation';
 export default async function Home() {
+  redirect('/dashboard')
   return (
-    <Button>Click</Button>
+    <div></div>
   )
 }
